010ca62b26f7deffc31b259a9b46f7b002fc4876
Seomingyun <aa123490@gmail.com>
2023-01-25 16:22:34 +0900

    fix&add asset functions

--- contracts/Product.sol
+++ contracts/Product.sol
@@ -12,24 +12,11 @@ abstract contract Product is ERC20, IProduct {
 
     AssetParams[] public assets;
     StrategyParams[] public strategies;
-    uint256 floatRatio;
+    uint256 floatRatio; // 10만분율
 
     string private _dacName; // DB가 없어서 contract에서도 필요한 듯
     address private _dacAddress; // 애초에 필요
     uint256 private _sinceDate;
-
-    // struct AssetParams {
-    //     address assetAddress;
-    //     address oracleAddress; // for chainlink price feed
-    //     uint256 targetWeight; // TODO type 조정 필요, 소숫점 관리 어떻게 할지 논의 필요. if targetWeight 25 => 이후 로직에서 100으로 항상 나눠줘야 함.
-    //     uint256 currentPrice; // when rebalancing -> update
-    // }
-
-    // struct StrategyParams {
-    //     address strategyAddress;
-    //     address assetAddress;
-    //     uint256 assetBalance;
-    // }
 
     modifier onlyDac{
         require(_msgSender()==_dacAddress);
@@ -41,13 +28,18 @@ abstract contract Product is ERC20, IProduct {
         string memory symbol_, 
         string memory dacName_, 
         address dacAddress_, 
-        AssetParams[] memory assets_
+        AssetParams[] memory assets_, // 괜찮은가 ?
+        uint256 floatRatio_
         ) 
         ERC20(name_, symbol_)
     {
         _dacName = dacName_;
+        require(dacAddress_ != address(0x0), "Invalid dac address");
         _dacAddress = dacAddress_;
         _sinceDate = block.timestamp;
+        assets = assets_;
+        require((floatRatio_ < 0) || (floatRatio_ > 100000), "Invalid float ratio");
+        floatRatio = floatRatio_;
     }
 
     function decimals() public view virtual override(ERC20, IERC20Metadata) returns (uint8) {
@@ -86,33 +78,15 @@ abstract contract Product is ERC20, IProduct {
         return false;
     }
 
-    // (민균) 이게 전체 float + strategy 인듯 ? (dollar 기준)
-    function getPortfolioValue() public view returns (uint256) {
-        uint256 portfolioValue = 0;
-        for (uint256 i=0; i < assets.length; i++) {
-            portfolioValue += getAssetValue(assets[i].assetAddress);
-        }
-        return portfolioValue;
+    ///@notice float (amount)
+    function assetFloatBalance(address assetAddress) public view override returns(uint256) {
+        require(checkAsset(assetAddress), "Asset Doesn't Exist");
+        return IERC20(assetAddress).balanceOf(address(this));
     }
 
-    // (민균) asset 1개 당 float + strategy (dollar 기준)
-    function getAssetValue(address assetAddress) public view returns (uint256) {
-        require(checkAsset(assetAddress), "Asset Doesn't Exist");
-
-        // TODO get asset values of each asset
-        // considering float and assets in strategy
-        // 1. get asset amount from strategies related
-            // strategies의 totalAsset() 불러오는지 결정 필요. 만약 전자라면 언제 Param 업데이트 해아하는지 결정 필요.
-        // 2. add it the float amount of that token
-        // uint256 floatAmount = balanceOfAsset(assetAddress);
-        // 3. get values by multiplying the asset price
-    }
-
-    /// @notice Token Amount of float token corresponding to param. 
-    function balanceOfAsset(address assetAddress) public view returns (uint256) {
-        require(checkAsset(assetAddress), "Asset Doesn't Exist");
-        // uint256 strategyAsset
-        uint256 totalBalance = IERC20(assetAddress).balanceOf(address(this));
+    ///@notice strategy + float (1 asset, amount)
+    function assetBalance(address assetAddress) public view override returns(uint256) {
+        uint256 totalBalance = assetFloatBalance(assetAddress);
         for (uint i = 0; i < strategies.length; i++) {
             if(strategies[i].assetAddress == assetAddress) {
                 totalBalance += IStrategy(strategies[i].strategyAddress).totalAssets();
@@ -121,17 +95,62 @@ abstract contract Product is ERC20, IProduct {
         return totalBalance;
     }
 
+    ///@notice stratey + float (dollar)
+    function portfolioValue() public view override returns(uint256) {
+        uint256 totalValue = 0;
+        for (uint256 i=0; i<assets.length; i++) {
+            totalValue += assetBalance(assets[i].assetAddress) * ChainlinkGateway.getLatestPrice(assets[i].oracleAddress);
+        }
+        return totalValue;
+    }
+
+    ///@notice strategy + float (1 asset, dollar)
+    function assetValue(address assetAddress) public view override returns (uint256) {
+        uint totalValue = 0;
+        for (uint256 i=0; i < assets.length; i++) {
+            if(assets[i].assetAddress == assetAddress) {
+                totalValue += assetBalance(assets[i].assetAddress) * ChainlinkGateway.getLatestPrice(assets[i].oracleAddress);
+                break;
+            }
+        }
+        return totalValue;
+    }
+
+    ///@notice float (all asset, dollar)
+    function totalFloatValue() public view override returns (uint256) {
+        uint256 totalValue = 0;
+        for (uint256 i=0; i<assets.length; i++) {
+            totalValue += assetFloatBalance(assets[i].assetAddress) * ChainlinkGateway.getLatestPrice(assets[i].oracleAddress);
+        }
+        return totalValue;
+    }
+
     function addAsset(address newAssetAddress, address newOracleAddress) external {
         require(!checkAsset(newAssetAddress), "Asset Already Exists");
         assets.push(AssetParams(newAssetAddress, newOracleAddress, 0, 0)); 
     }
 
-    function updateWeight(AssetParams[] memory newParams) public {
-        for (uint i = 0; i < newParams.length; i++) {
+    function updateWeight(address[] memory assetAddresses, uint256[] memory assetWeights) public {
+        for (uint i = 0; i < assetAddresses.length; i++) {
             bool found = false;
             for (uint j = 0; j < assets.length; j++) {
-                if(assets[j].assetAddress == newParams[i].assetAddress) {
-                    assets[j] = newParams[i]; // (민균) current price에 혼동이 오려나 ? 그냥 weight만 update하는 방식으로 바꿔야 하나 싶음
+                if(assets[j].assetAddress == assetAddresses[i]) {
+                    require((assetWeights[i] >= 0) || (assetWeights[i] <= 100000), "Invalid asset target weight"); // 10만분율
+                    assets[j].targetWeight = assetWeights[i];
+                    found = true;
+                    break;
+                    }
+                }
+            require(found, "Asset not found");
+        }
+    }
+
+    function updateOracleAddress(address[] memory assetAddresses, address[] memory assetOracles) public {
+        for (uint i = 0; i < assetAddresses.length; i++) {
+            bool found = false;
+            for (uint j = 0; j < assets.length; j++) {
+                if(assets[j].assetAddress == assetAddresses[i]) {
+                    assets[j].oracleAddress = assetOracles[i];
                     found = true;
                     break;
                     }
@@ -172,15 +191,14 @@ abstract contract Product is ERC20, IProduct {
         uint256 portfolioValue = 0;
         for (uint i = 0; i < assets.length; i++) {
             assets[i].currentPrice = ChainlinkGateway.getLatestPrice(assets[i].assetAddress);
-            portfolioValue += getAssetValue(assets[i].assetAddress);
+            portfolioValue += assetValue(assets[i].assetAddress); // stratey + float value
         }
 
         for(uint i=0; i < assets.length; i++){
             uint256 targetBalance = (assets[i].targetWeight * portfolioValue) / assets[i].currentPrice;
-            uint256 currentBalance = balanceOfAsset(assets[i].assetAddress);
+            uint256 currentBalance = assetFloatBalance(assets[i].assetAddress); // float balance
             if (currentBalance > targetBalance) {
                 // Sell
-                // float으로 충분할 경우 // (민균) 지금 currentBalance가 float에 대한 양이 아니지 않나요 ?.. float + strategy 인뎅
                 uint256 sellAmount = currentBalance - targetBalance;
                 
                 // float으로 부족할 경우
@@ -204,17 +222,12 @@ abstract contract Product is ERC20, IProduct {
         // emit Rebalance(block.timestamp);
     }
 
-    // function totalSupply() external view returns (uint256); // erc20 function
-    // function balanceOf(address owner) external view returns (uint256); // erc20 function
-
-
     function maxDeposit(address receiver) external view override returns (uint256){} // for deposit
     function maxWithdraw(address owner) external view override returns (uint256){} // for withdraw
 
     // strategy와 상호작용
     function depositIntoStrategy(address strategyAddress, uint256 assetAmount) external override {} 
     function redeemFromStrategy(address strategyAddress, uint256 assetAmount) external override {}
-
 
     // 보류
     function convertToShares(uint256 assetAmount) external view override returns(uint256 shareAmount) {}
