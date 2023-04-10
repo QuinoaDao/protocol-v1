944399b6636d2fed4ab043c2da9dda98d3919481
Seomingyun <aa123490@gmail.com>
2023-04-10 19:59:04 +0900

    add interface for balancer interacting

--- contracts/interfaces/IBalancerVault.sol
+++ contracts/interfaces/IBalancerVault.sol
@@ -14,11 +14,25 @@ interface IBalancerVault {
         bool fromInternalBalance;
     }
 
+    struct ExitPoolRequest {
+        address[] assets;
+        uint256[] minAmountsOut;
+        bytes userData;
+        bool toInternalBalance;
+    }
+
     function joinPool(
         bytes32 poolId,
         address sender,
         address recipient,
         JoinPoolRequest memory request
+    ) external;
+
+    function exitPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        ExitPoolRequest memory request
     ) external;
 
     function getPool(bytes32 poolId)
