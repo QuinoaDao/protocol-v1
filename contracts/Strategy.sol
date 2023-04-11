2c0fa99938bb9168b4b45f2786f0cae73e7371bf
Seomingyun <aa123490@gmail.com>
2023-04-11 15:52:28 +0900

    modify the function name to match the changes in IStrategy.sol

--- contracts/Strategy.sol
+++ contracts/Strategy.sol
@@ -8,51 +8,58 @@ import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Strategy is IStrategy {
 
-    address _underlyingAsset;
-    address _dacAddress;
-    address _productAddress;
+    // strategy state variables
+    address public dac;
+    address public product;
+    address immutable public underlyingAsset;
 
     modifier onlyProduct {
-        require(msg.sender == _productAddress, "No permission");
+        require(msg.sender == product, "No permission: only product");
         _;
     }
 
-    constructor (address dacAddress_, address underlyingAsset_, address productAddress_) {
-        _dacAddress = dacAddress_;
+    modifier onlyDac {
+        require(msg.sender == dac, "No permission: only dac");
+        _;
+    }
+
+    constructor (address dac_, address underlyingAsset_,address product_) {
+        require(dac_ != address(0x0), "Invalid dac address");
+        dac = dac_;
 
         require(underlyingAsset_ != address(0x0), "Invalid underlying asset address");
-        _underlyingAsset = underlyingAsset_;
+        underlyingAsset = underlyingAsset_;
 
-        require(productAddress_ != address(0x0), "Invalid product address");
-        _productAddress = productAddress_;
-    }
-
-    function dacAddress() public view override returns(address) {
-        return _dacAddress;
-    }
-
-    function underlyingAsset() external view override returns(address) {
-        return _underlyingAsset;
+        require(product_ != address(0x0), "Invalid product address");
+        product = product_;
     }
 
     function totalAssets() public view override returns(uint256) {
-        return IERC20(_underlyingAsset).balanceOf(address(this));
+        return IERC20(underlyingAsset).balanceOf(address(this));
     }
 
-    function withdrawToProduct(uint256 assetAmount) external override onlyProduct returns(bool) {
-        if(assetAmount > 0) SafeERC20.safeTransfer(IERC20(_underlyingAsset), _productAddress, assetAmount);
+    function withdraw(uint256 assetAmount) external override onlyProduct returns(bool) {
+        if(assetAmount > 0) SafeERC20.safeTransfer(IERC20(underlyingAsset), product, assetAmount);
         return true;
     }
+
+    function withdrawAll() external onlyProduct returns(bool) {
+        // If product is in activation state, Dac cannot call this method
+        require(!IProduct(product).checkActivation(), "Product is active now");
+        SafeERC20.safeTransfer(IERC20(underlyingAsset), product, totalAssets());
+        return true;
+    }
+
 
     // Todo: this strategy have no real strategy logic
-    function depositToDelegate() external returns(bool) {
-        return true;
+    function deposit() external override {
     }
 
-    function withdrawAllToProduct() external onlyProduct returns(bool) {
-        // If product is in activation state, Dac cannot call this method
-        require(!IProduct(_productAddress).checkActivation(), "Product is active now");
-        SafeERC20.safeTransfer(IERC20(_underlyingAsset), _productAddress, totalAssets());
-        return true;
+    function delegate() external pure override returns(address) {
+        return address(0);
+    }
+
+    function yield() external pure override returns(address) {
+        return address(0);
     }
 }
